85047f726b416c70720780972a608b9ba98170c6
Toivo Toivo <toivotauno0321@gmail.com>
2025-08-19 03:09:35 +0200

    add changes

--- sql/livestock_db.sql
+++ sql/livestock_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2025 at 12:19 PM
+-- Generation Time: Aug 19, 2025 at 03:06 AM
 -- Server version: 8.0.34
 -- PHP Version: 8.2.12
 
@@ -63,7 +63,8 @@ CREATE TABLE `livestock` (
 INSERT INTO `livestock` (`id`, `species`, `birth_date`, `owner_id`, `animal_id`, `breed`, `gender`, `color`, `weight`, `location`, `notes`, `created_at`) VALUES
 (1, 'cattle', '2025-08-11', 7, '1485', 'hereford', 'male', 'black', '680', 'Windhoek', 'Healthy', '2025-08-11 12:27:46'),
 (2, 'sheep', '2025-08-05', 8, '1486', 'Dorper', 'female', 'white', '100', '', 'heealthy', '2025-08-12 03:55:40'),
-(3, 'goat', '2025-08-11', 8, '1488', 'boer', 'male', 'black', '140', 'swakopmund', 'greatest of all time', '2025-08-12 03:57:36');
+(3, 'goat', '2025-08-11', 8, '1488', 'boer', 'male', 'black', '140', 'swakopmund', 'greatest of all time', '2025-08-12 03:57:36'),
+(4, 'chicken', '2025-08-12', 8, '1499', 'silkie', 'male', 'yellow', '30', 'Ohangwena', 'fast', '2025-08-12 12:30:29');
 
 -- --------------------------------------------------------
 
@@ -88,8 +89,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `Email`, `phone`, `full_name`, `created_at`) VALUES
 (4, 'farmer1', '$2y$10$V...hashedStringHere...', 'farmer', NULL, NULL, NULL, '2025-08-10 18:32:45'),
-(5, 'vet1', '$2y$10$V...hashedStringHere...', 'vet', NULL, NULL, NULL, '2025-08-10 18:32:45'),
-(6, 'admin1', '$2y$10$V...hashedStringHere...', 'admin', NULL, NULL, NULL, '2025-08-10 18:32:45'),
+(5, 'vet1', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'vet', NULL, NULL, NULL, '2025-08-10 18:32:45'),
+(6, 'admin1', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', NULL, NULL, NULL, '2025-08-10 18:32:45'),
 (7, 'jr_toivo', '$2y$10$R17JAW75.uXOd0hHbKOSc.dyZ4VXb.Dkw7AhJAD6A0VKLnJo1rgJq', 'extension_officer', 'toivotauno0321@gmail.com', '+264815605924', 'Tauno ', '2025-08-10 18:32:49'),
 (8, 'MK', '$2y$10$IKVT7Wi1yNF7EVhcGZw6i.gUZQebuZXzRMY6WN7f6Sr12g11aNfRO', 'farmer', 'moanakalupeteka@gmail.com', '+264812345678', 'Moana Kalupeteka', '2025-08-11 20:14:52'),
 (9, 'JB', '$2y$10$SG7j.CZdDZ.HYiBLYT.LieZDuLx1xq30YHpndT2Idx64aUWIX6Pf6', 'vet', 'jb@gmail.com', '+264818765432', 'James Bond', '2025-08-12 01:08:12');
@@ -103,7 +104,7 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`, `Email`, `phone`, `fu
 CREATE TABLE `vaccinations` (
   `id` int NOT NULL,
   `type` varchar(100) NOT NULL,
-  `date_administered` date NOT NULL,
+  `date_administered` date DEFAULT NULL,
   `due_date` date DEFAULT NULL,
   `next_due_date` date DEFAULT NULL,
   `batch_number` varchar(50) DEFAULT NULL,
@@ -161,7 +162,7 @@ ALTER TABLE `disease_reports`
 -- AUTO_INCREMENT for table `livestock`
 --
 ALTER TABLE `livestock`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
